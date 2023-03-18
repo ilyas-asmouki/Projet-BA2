2e094e0226a9e6e74f2288c6a544a8af87643238
ilyas-asmouki <128220841+ilyas-asmouki@users.noreply.github.com>
2023-03-18 23:54:12 +0100

    Update shape.h

--- shape.h
+++ shape.h
@@ -4,13 +4,13 @@
 #include <iostream>
 #include <cmath>
 
-enum MODE {LECTURE , SIMULATION};
+enum MODE {LECTURE, SIMULATION};
 constexpr double epsil_zero(0.125);
 
 struct S2d
 {
-	double x = 0;
-	double y = 0;
+	double x;
+	double y;
 };
 
 struct Cercle
@@ -27,9 +27,9 @@ struct Carre
 
 double distance(S2d p1, S2d p2);
 double norme(S2d vect);
-bool superposition_carres(Carre s1, Carre s2, MODE mode);
+bool superposition_carres(const Carre s1, const Carre s2, MODE mode);
 bool superposition_cercles(Cercle c1, Cercle  c2, MODE mode);
-bool superposition_cerclecarre(Cercle c2, Carre c1, MODE mode);
+bool superposition_cerclecarre(Carre c1, Cercle c2, MODE mode);
 
 #endif
 
