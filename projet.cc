730813330c1ac664ee1b00fd1617c19bad9b4897
saadl27 <128222433+saadl27@users.noreply.github.com>
2023-03-18 19:44:52 +0100

    Update projet.cc

--- projet.cc
+++ projet.cc
@@ -1,7 +1,20 @@
 #include "simulation.h"
+#include <iostream>
 using namespace std;
 
-int main()
+void erreur_fichier(int argc);
+
+int main(int argc, char* argv[])
 {	
+	erreur_fichier(argc);
+	Simulation Propre_en_Ordre;
+	Propre_en_Ordre.lecture(argv[2]);
 	return 0;
+}
+
+void erreur_fichier(int argc){
+	if (argc == 1){
+		exit(EXIT_FAILURE);
+	}
+	return;
 }
