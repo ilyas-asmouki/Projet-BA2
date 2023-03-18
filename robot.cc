08eaa575f930c8e95359db43b5048d3ca8cbc6df
saadl27 <128222433+saadl27@users.noreply.github.com>
2023-03-18 11:19:35 +0100

    Update robot.cc

--- robot.cc
+++ robot.cc
@@ -1,7 +1,5 @@
-#ifndef ROBOT_H
-#define ROBOT_H
 #include "robot.h"
-#endif
+#include <vector>
 
 #ifndef PARTICULE_H
 #define PARTICULE_H
@@ -19,3 +17,29 @@
 #endif
 
 using namespace std;
+static vector<Robot> tab_robot;
+
+Robot::Robot(double x1=0, double y1=0) : forme({{x1,y1}, 0})
+
+{}
+ 
+Spatial::Spatial(double x, double y, unsigned a ,unsigned b, unsigned c, 
+ unsigned d ,unsigned e): Robot(x, y), nbUpdate(a), nbNr(b), nbNd(c), nbRr(d), nbRs(e)
+{
+	forme.rayon=r_spatial;
+	tab_robot.push_back(*this);
+}
+	
+Reparateur::Reparateur(double x, double y): Robot(x, y)
+{
+	forme.rayon=r_reparateur; 
+	tab_robot.push_back(*this);
+}
+		
+Neutraliseur::Neutraliseur(double x, double y, int a, unsigned b, bool c, unsigned d)
+ : Robot(x, y), orientation(a), type(b), panne(c), k_update_panne(d) 
+{
+	forme.rayon=r_neutraliseur;
+	tab_robot.push_back(*this);
+}
+		
