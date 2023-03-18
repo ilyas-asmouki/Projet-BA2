fa9a558c6842218887773b3086545edde2b298e3
ilyas-asmouki <128220841+ilyas-asmouki@users.noreply.github.com>
2023-03-18 23:55:20 +0100

    Update particule.h

--- particule.h
+++ particule.h
@@ -3,29 +3,6 @@
 
 #include "shape.h"
 #include <vector>
-
-constexpr double d_particule_min(8*epsil_zero);
-constexpr double dmax(128.);
-
-class Particule
-{
-    private:
-    Carre forme;
-    public:
-    Particule(double x, double y, double cote);
-    Carre getForme() const;
-    void detect_particle_outside();
-    void detect_particle_too_small();
-    void detect_particle_superposition(); 
-};
-
-static std::vector<Particule> tab_particule;
-
-void #ifndef PARTICULE_H
-#define PARTICULE_H
-
-#include "shape.h"
-#include <vector>
 #include <sstream>
 
 constexpr double d_particule_min(8*epsil_zero);
@@ -46,10 +23,6 @@ class Particule
 static std::vector<Particule> tab_particule;
 
 void decodage_particule(std::istringstream& data);
-
-#endif
-
-(std::istringstream& data);
 
 #endif
 
