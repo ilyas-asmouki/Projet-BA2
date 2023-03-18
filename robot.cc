7bf02ddaa7d20372a66cf2b2379bcdd3f5a2629d
ilyas-asmouki <128220841+ilyas-asmouki@users.noreply.github.com>
2023-03-18 17:47:28 +0100

    Update robot.cc

--- robot.cc
+++ robot.cc
@@ -20,23 +20,25 @@
 using namespace std;
 static vector<Robot> tab_robot;
 
-Robot::Robot(double x1=0, double y1=0) : forme({{x1,y1}, 0})
+Robot::Robot(double x = 0, double y = 0) : forme({{x, y}, 0})
 {}
 
 Cercle Robot::getforme(){
 	return forme;
 }
  
-Spatial::Spatial(double x, double y, unsigned a ,unsigned b, unsigned c, 
- unsigned d ,unsigned e): Robot(x, y), nbUpdate(a), nbNr(b), nbNd(c), nbRr(d), nbRs(e)
+Spatial::Spatial(double x, double y, unsigned nbUpdate, unsigned nbNr, unsigned nbNd, 
+unsigned nbRr, unsigned nbRs): Robot(x, y), nbUpdate(nbUpdate), nbNr(nbNr),
+nbNd(nbNd), nbRr(nbRr), nbRs(nbRs)
 {
-	forme.rayon=r_spatial;
+	forme.rayon = r_spatial;
 	error_outside();
 }
 	
 void Spatial::error_outside() {
-	if ((abs(forme.centre.x+r_spatial)>dmax)or(abs(forme.centre.y+r_spatial)>dmax)){
-		cout<<message::spatial_robot_ouside(forme.centre.x, forme.centre.y);
+	if ((abs(forme.centre.x+r_spatial)>dmax)or(abs(forme.centre.y+r_spatial)>dmax))
+	{
+		cout << message::spatial_robot_ouside(forme.centre.x, forme.centre.y);
 		exit(EXIT_FAILURE);
 	}
 	return;
@@ -44,24 +46,26 @@ void Spatial::error_outside() {
 
 Reparateur::Reparateur(double x, double y): Robot(x, y)
 {
-	forme.rayon=r_reparateur; 
+	forme.rayon = r_reparateur; 
 	TestCollision();
 	tab_robot.push_back(*this);
 }
 		
-Neutraliseur::Neutraliseur(double x, double y, int a, unsigned b, bool c, 
-                           unsigned d, unsigned nbUpdate)
- : Robot(x, y), orientation(a), type(b), panne(c), k_update_panne(d) 
+Neutraliseur::Neutraliseur(double x, double y, int orientation, unsigned type,
+						   bool panne,  unsigned k_update, unsigned nbUpdate)
+: Robot(x, y), orientation(orientation), type(type), panne(panne), 
+k_update_panne(k_update) 
 {
-	forme.rayon=r_neutraliseur;
+	forme.rayon = r_neutraliseur;
 	error_k_update(nbUpdate);
 	TestCollision();
 	tab_robot.push_back(*this);
 }
 	
 void Neutraliseur::error_k_update(unsigned nbUpdate){
-	if (k_update_panne > nbUpdate){
-		cout<<message::invalid_k_update(forme.centre.x, forme.centre.y, 
+	if (k_update_panne > nbUpdate)
+	{
+		cout << message::invalid_k_update(forme.centre.x, forme.centre.y, 
 		                                k_update_panne, nbUpdate);
 		exit(EXIT_FAILURE);
 	}
@@ -70,20 +74,20 @@ void Neutraliseur::error_k_update(unsigned nbUpdate){
 	
 	
 void Robot::TestCollision() {
-	for (size_t i(0); i < tab_robot.size(); ++i) {
+	for (size_t i = 0; i < tab_robot.size(); ++i) {
 		if (superposition_cercles(forme, tab_robot[i].getforme(),LECTURE)){
 			if (forme.rayon == tab_robot[i].getforme().rayon) {
 				if (forme.rayon == r_reparateur) {
-					cout<<message::repairers_superposition(forme.centre.x , 	
+					cout << message::repairers_superposition(forme.centre.x , 	
 					tab_robot[i].getforme().centre.x, forme.centre.y ,
 					tab_robot[i].getforme().centre.y);
 				} else { 
-					cout<<message::neutralizers_superposition(
+					cout << message::neutralizers_superposition(
 					forme.centre.x, tab_robot[i].getforme().centre.x, 
 					forme.centre.y , tab_robot[i].forme.centre.y);	
 				}
 			} else {
-				cout<<message::repairer_neutralizer_superposition(
+				cout << message::repairer_neutralizer_superposition(
 				forme.centre.x , tab_robot[i].getforme().centre.x, 
 				forme.centre.y ,tab_robot[i].getforme().centre.y);
 			}
@@ -92,5 +96,20 @@ void Robot::TestCollision() {
 	}
 	return;
 }	
-		
-void decodage_robot(std::string v){	
+
+void Robot::test_particle_robot_superposition()
+{
+	for (size_t i = 0; i < tab_particule.size(); ++i)
+	{
+		if (superposition_cerclecarre(forme, tab_particule[i].getForme(), LECTURE))
+		{
+			cout << message::particle_robot_superposition(tab_particule[i].getForme().
+			centre.x,tab_particule[i].getForme().centre.y,tab_particule[i].getForme().
+			cote, forme.centre.x, forme.centre.y, forme.rayon);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+
+
+
