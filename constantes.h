5600010188b78f681334fd63a181bd52614cb54f
ilyas-asmouki <128220841+ilyas-asmouki@users.noreply.github.com>
2023-03-18 11:40:18 +0100

    Add files via upload

---
+++ constantes.h
@@ -0,0 +1,20 @@
+#include "shape.h"
+enum Etat_neutralisateur{EN_PANNE, EN_MARCHE};
+
+constexpr short unsigned maxF(25);
+
+constexpr double dmax(128.);
+constexpr double delta_t(0.125);  //seconde
+constexpr double r_spatial(16.);
+constexpr double r_reparateur(2.);
+constexpr double r_neutraliseur(4.);
+constexpr double vtran_max(4.);            //par seconde
+constexpr double vrot_max(0.125);          //rd/s     env. 7°/s
+constexpr double epsil_alignement(0.01);   //rd       env. 0.6°
+
+constexpr double desintegration_rate(0.0002);
+constexpr double risk_factor(3.);
+constexpr double d_particule_min(8*shape::epsil_zero);
+
+constexpr unsigned max_update(600);
+constexpr unsigned modulo_update(100);
