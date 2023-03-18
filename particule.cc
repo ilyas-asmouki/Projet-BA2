3ca8b316f9dd152a2143496c237e3955d065dc53
ilyas-asmouki <128220841+ilyas-asmouki@users.noreply.github.com>
2023-03-18 16:46:34 +0100

    Update particule.cc

--- particule.cc
+++ particule.cc
@@ -1,12 +1,9 @@
-#ifndef PARTICULE_H
-#define PARTICULE_H
 #include "particule.h"
-#endif
 
-#ifndef MESSAGE_H_INCLUDED
-#define MESSAGE_H_INCLUDED
+//~ #ifndef MESSAGE_H_INCLUDED
+//~ #define MESSAGE_H_INCLUDED
 #include "message.h"
-#endif
+//~ #endif
 
 #ifndef SHAPE_H
 #define SHAPE_H
@@ -14,3 +11,46 @@
 #endif
 
 using namespace std;
+
+Particule::Particule(double x, double y, double cote) : forme({{x,y},cote})
+{
+    detect_particle_outside(x, y, cote);
+    detect_particle_too_small(x, y, cote);
+    tab_particule.push_back(*this);
+}
+
+Carre Particule::getForme() const
+{
+	return forme;
+}
+
+void detect_particle_outside(double x, double y, double s)
+{
+	if (abs(x) + s / 2 > dmax or abs(y) + s / 2 > dmax)
+    {
+        cout << message::particle_outside(x, y, s);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void detect_particle_too_small(double x, double y, double s)
+{
+	 if (s < d_particule_min)
+    {
+        cout << message::particle_too_small(x, y, s);
+        exit(EXIT_FAILURE);
+    }
+}
+
+void Particule::detect_particle_superposition()
+{
+	for (size_t i = 0; i < tab_particule.size(); ++i)
+	{
+		if (superposition_carres(forme, tab_particule[i].getForme(), LECTURE))
+		{
+			cout << message::particle_superposition(forme.centre.x, forme.centre.y,
+			tab_particule[i].getForme().centre.x,tab_particule[i].getForme().centre.y);
+			exit(EXIT_FAILURE);
+		}
+	}
+}
