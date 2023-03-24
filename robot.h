6783a57b904a83ebf454059d30c75a9fededd3f0
ilyas-asmouki <128220841+ilyas-asmouki@users.noreply.github.com>
2023-03-24 16:35:03 +0100

    Update robot.h

--- robot.h
+++ robot.h
@@ -10,44 +10,48 @@ constexpr double r_spatial(16.);
 constexpr double r_reparateur(2.);
 constexpr double r_neutraliseur(4.);
 
-class Robot {
+class Robot
+{
 	protected : 
-	 Cercle forme;
+	Cercle forme;
 	public :
-	 Robot(double x, double y);
-	 Cercle getforme();
-	 void TestCollision();
+	Robot(double x, double y);
+	Cercle getforme();
+	void TestCollision();
 };
 
-class Spatial : public Robot {
+class Spatial : public Robot
+{
 	private :
-	 int nbUpdate;
-	 unsigned nbNr;
-	 unsigned nbNs;
-	 unsigned nbNd;
-	 unsigned nbRr;
-	 unsigned nbRs;
+	int nbUpdate;
+	unsigned nbNr;
+	unsigned nbNs;
+	unsigned nbNd;
+	unsigned nbRr;
+	unsigned nbRs;
 	public:
-	 Spatial(double x, double y, int nbUpdate ,unsigned nbNr, unsigned nbNs,
-	  unsigned nbNd, unsigned nbRr ,unsigned nbRs);
-	 void error_outside();
+	Spatial(double x, double y, int nbUpdate ,unsigned nbNr, unsigned nbNs,
+	unsigned nbNd, unsigned nbRr ,unsigned nbRs);
+	void error_outside();
 };
 
-class Reparateur : public Robot {
+class Reparateur : public Robot
+{
 	private :
 	public :
-	 Reparateur(double x, double y);
+	Reparateur(double x, double y);
 };
 
-class Neutraliseur : public Robot {
+class Neutraliseur : public Robot
+{
 	private :
-	 double  orientation;
-	 unsigned type;
-	 bool panne;
-	 int k_update_panne;
+	double  orientation;
+	unsigned type;
+	bool panne;
+	int k_update_panne;
 	public :
-	 Neutraliseur(double x, double y, double a, unsigned b, std::string c, int nbUpdate, int  d);
-	 void error_k_update(int nbUpdate);
+	Neutraliseur(double x, double y, double a, unsigned b, std::string c, int nbUpdate, int  d);
+	void error_k_update(int nbUpdate);
 };
 
 void decodage_robot(std::istringstream& v, int n, int& compteur1, int& compteur2);
