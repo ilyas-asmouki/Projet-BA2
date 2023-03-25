a3e8e53793405e823bbcd39cae6a874ef708322f
saadl27 <128222433+saadl27@users.noreply.github.com>
2023-03-25 12:29:52 +0100

    Update shape.cc

--- shape.cc
+++ shape.cc
@@ -10,19 +10,19 @@ double norme(S2d vect)
 	return sqrt(pow(vect.x, 2) + pow(vect.y, 2));
 }
 
-bool superposition_carres(const Carre s1, const Carre s2, MODE mode)
+bool superposition_carres(const Carre s1, const Carre s2, MARGIN margin)
 {
 	double d1 = s1.cote, d2 = s2.cote;
-	return (abs(s2.centre.x - s1.centre.x) < d1 / 2 + d2 / 2 + shape::epsil_zero*mode)
-		and (abs(s2.centre.y - s1.centre.y) < d1 / 2 + d2 / 2 + shape::epsil_zero*mode);
+	return (abs(s2.centre.x - s1.centre.x) < d1 / 2 + d2 / 2 + shape::epsil_zero*margin)
+		and (abs(s2.centre.y - s1.centre.y) < d1 / 2 + d2 / 2 + shape::epsil_zero*margin);
 }
 
-bool superposition_cercles(Cercle c1, Cercle  c2, MODE mode)
+bool superposition_cercles(Cercle c1, Cercle  c2, MARGIN margin)
 {
-    return ((c1.rayon+c2.rayon+shape::epsil_zero*mode)>distance(c1.centre, c2.centre));
+    return ((c1.rayon+c2.rayon+shape::epsil_zero*margin)>distance(c1.centre, c2.centre));
 }
 
-bool superposition_cerclecarre(Carre c1, Cercle c2, MODE mode)
+bool superposition_cerclecarre(Carre c1, Cercle c2, MARGIN margin)
 {
 	double x1 = c1.centre.x, y1 = c1.centre.y, d1 = c1.cote;
 	double x2 = c2.centre.x, y2 = c2.centre.y, r2 = c2.rayon;
@@ -32,11 +32,10 @@ bool superposition_cerclecarre(Carre c1, Cercle c2, MODE mode)
     double L = norme(vect);
     
     if ((abs(x2 - x1) > d1 / 2) and (abs(y2 - y1) > d1 / 2)
-    and (L > r2 + shape::epsil_zero*mode))
+    and (L > r2 + shape::epsil_zero*margin))
 		return false;
-	else if ((abs(x2 - x1) < d1 / 2 + r2 + shape::epsil_zero*mode)
-	and (abs(y2 - y1) < d1 / 2 + r2 + shape::epsil_zero*mode))
+	else if ((abs(x2 - x1) < d1 / 2 + r2 + shape::epsil_zero*margin)
+	and (abs(y2 - y1) < d1 / 2 + r2 + shape::epsil_zero*margin))
 		return true;
 	return false;
 }
-
