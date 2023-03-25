c99b820b4dee6bdcd154d895c2959e199f351afa
saadl27 <128222433+saadl27@users.noreply.github.com>
2023-03-25 12:28:23 +0100

    Update shape.h

--- shape.h
+++ shape.h
@@ -1,36 +1,41 @@
-#ifndef SHAPE_H
-#define SHAPE_H
+#include "shape.h"
 
-#include <iostream>
-#include <cmath>
-
-enum MODE {LECTURE, SIMULATION};
-
-namespace shape {
-	constexpr double epsil_zero(0.125);
+double distance(S2d p1, S2d p2)
+{
+    return sqrt((p2.x-p1.x) * (p2.x-p1.x) + (p2.y-p1.y) * (p2.y-p1.y));
 }
-struct S2d
+
+double norme(S2d vect)
 {
-	double x;
-	double y;
-};
+	return sqrt(pow(vect.x, 2) + pow(vect.y, 2));
+}
 
-struct Cercle
+bool superposition_carres(const Carre s1, const Carre s2, MARGIN margin)
 {
-	S2d centre;
-	double rayon;
-};
+	double d1 = s1.cote, d2 = s2.cote;
+	return (abs(s2.centre.x - s1.centre.x) < d1 / 2 + d2 / 2 + shape::epsil_zero*margin)
+		and (abs(s2.centre.y - s1.centre.y) < d1 / 2 + d2 / 2 + shape::epsil_zero*margin);
+}
 
-struct Carre
+bool superposition_cercles(Cercle c1, Cercle  c2, MARGIN margin)
 {
-	S2d centre;
-	double cote;
-};
+    return ((c1.rayon+c2.rayon+shape::epsil_zero*margin)>distance(c1.centre, c2.centre));
+}
 
-double distance(S2d p1, S2d p2);
-double norme(S2d vect);
-bool superposition_carres(const Carre s1, const Carre s2, MODE mode);
-bool superposition_cercles(Cercle c1, Cercle  c2, MODE mode);
-bool superposition_cerclecarre(Carre c1, Cercle c2, MODE mode);
-
-#endif
+bool superposition_cerclecarre(Carre c1, Cercle c2, MARGIN margin)
+{
+	double x1 = c1.centre.x, y1 = c1.centre.y, d1 = c1.cote;
+	double x2 = c2.centre.x, y2 = c2.centre.y, r2 = c2.rayon;
+	S2d vect;
+	vect.x =(abs(x2 - x1) - d1 / 2);
+	vect.y = (abs(y2 - y1) - d1 / 2);
+    double L = norme(vect);
+    
+    if ((abs(x2 - x1) > d1 / 2) and (abs(y2 - y1) > d1 / 2)
+    and (L > r2 + shape::epsil_zero*margin))
+		return false;
+	else if ((abs(x2 - x1) < d1 / 2 + r2 + shape::epsil_zero*margin)
+	and (abs(y2 - y1) < d1 / 2 + r2 + shape::epsil_zero*margin))
+		return true;
+	return false;
+}
