ec94e57df6826c96ee5e8070fab18a5720b7fe6f
saadl27 <128222433+saadl27@users.noreply.github.com>
2023-03-31 16:48:00 +0200

    Update particule.h

--- particule.h
+++ particule.h
@@ -1,28 +1,28 @@
+//LAHLOU SAAD 361150
+//ASMOUKI ILYAS 356263
+
 #ifndef PARTICULE_H
 #define PARTICULE_H
 
 #include "shape.h"
 #include <vector>
 #include <sstream>
-#include "constantes.h"
 
 class Particule
 {
+	public:
+     Particule(double x, double y, double cote);
+     Carre getForme() const;
+     void detect_particle_outside();
+     void detect_particle_too_small();
+     void detect_particle_superposition();
     private:
-    Carre forme;
-    public:
-    Particule(double x, double y, double cote);
-    Carre getForme() const;
-    void detect_particle_outside();
-    void detect_particle_too_small();
-    void detect_particle_superposition();
+     Carre forme;
 };
-
-void test_particle_robot_superposition(Cercle robot);
 
 static std::vector<Particule> tab_particule;
 
+void test_particle_robot_superposition(Cercle robot);
 void decodage_particule(std::istringstream& data);
 
 #endif
-
